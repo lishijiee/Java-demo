5982ba6239dff972103311ff5cb7819ecd7bb859
lishijiee <2424782003@qq.com>
2022-07-23 21:37:17 +0800

    Nio-Netty

---
+++ NIO-Netty/src/main/java/com/lishijieee/NioNettyApplication.java
@@ -0,0 +1,13 @@
+package com.lishijieee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NioNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NioNettyApplication.class, args);
+    }
+
+}
---
+++ NIO-Netty/src/test/java/com/lishijieee/NioNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lishijieee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NioNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
