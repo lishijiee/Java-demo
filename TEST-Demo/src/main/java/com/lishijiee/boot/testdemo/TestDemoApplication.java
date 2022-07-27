252eb20ae5d086ac4147c5c83c358a2764b49f30
lishijiee <2424782003@qq.com>
2022-07-27 13:54:34 +0800

    打印日志到指定位置

--- TEST-Demo/src/main/java/com/lishijiee/boot/testdemo/TestDemoApplication.java
+++ TEST-Demo/src/main/java/com/lishijiee/boot/testdemo/TestDemoApplication.java
@@ -1,12 +1,15 @@
 package com.lishijiee.boot.testdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class TestDemoApplication {
 
     public static void main(String[] args) {
+        log.info("项目启动成功！");
         SpringApplication.run(TestDemoApplication.class, args);
     }
 
